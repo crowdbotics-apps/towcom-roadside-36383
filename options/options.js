01bcafef8c79fbfac8d7995196ad59719bb5f5e3
Crowdbotics <team@crowdbotics.com>
2022-07-24 07:46:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "towcom_roadside_36383",
+  url: "https://towcom_roadside_36383.botics.co",
+  api: "https://towcom_roadside_36383.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
